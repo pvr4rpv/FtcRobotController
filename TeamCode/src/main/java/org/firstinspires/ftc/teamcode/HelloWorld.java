4cf6f23c67074b2d60abc34685297cf97c0619c2
pvr4rpv <pvr4rpv@gmail.com>
2025-09-20 16:26:38 -0700

    Update HelloWorld.java
    Added in my first tutorial class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -1,15 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
+@Autonomous
 public class HelloWorld extends OpMode {
 
     @Override
     public void init() {
         telemetry.addData("Hello","World");
     }
+
+    /*
+    1. Display Hello : "Your Name"
+    2. Run in autonomous mode
+     */
 
     @Override
     public void loop() {
